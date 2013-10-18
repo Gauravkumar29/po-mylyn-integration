0cf9612b18e8740d6cd8c4659d5aa04a94996f30
Markus Knittig <markus@myd0.de>
2013-10-18 16:03:43 +0200

    Icons & Bugfix

--- com.project_open.mylyn.ui/src/com/project_open/mylyn/ui/editor/TicketEditorPage.java
+++ com.project_open.mylyn.ui/src/com/project_open/mylyn/ui/editor/TicketEditorPage.java
@@ -9,8 +9,10 @@ import org.eclipse.core.runtime.Status;
 import org.eclipse.core.runtime.jobs.IJobChangeEvent;
 import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.core.runtime.jobs.JobChangeAdapter;
-import org.eclipse.mylyn.tasks.ui.editors.AbstractTaskEditorPage;
+import org.eclipse.mylyn.tasks.core.ITask;
+import org.eclipse.mylyn.tasks.core.TaskRepository;
 import org.eclipse.mylyn.tasks.ui.editors.TaskEditor;
+import org.eclipse.mylyn.tasks.ui.editors.TaskFormPage;
 import org.eclipse.osgi.util.NLS;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
@@ -29,118 +31,136 @@ import com.project_open.mylyn.ui.ProjectOpenUiPlugin;
 
 /**
  * @author Markus Knittig
- *
+ * 
  */
-public class TicketEditorPage extends AbstractTaskEditorPage {
+public class TicketEditorPage extends TaskFormPage {
 
-    private TaskEditor editor;
-    private ScrolledForm form;
-    private FormToolkit toolkit;
-    private Composite editorComposite;
+	private TaskEditor editor;
+	private ScrolledForm form;
+	private FormToolkit toolkit;
+	private Composite editorComposite;
 
-    private List<AbstractFormPagePart> parts;
+	private List<AbstractFormPagePart> parts;
 
-    private Ticket ticket;
-    private ProjectOpenClient client;
+	private Ticket ticket;
+	private ProjectOpenClient client;
 
-    public TicketEditorPage(TaskEditor editor, String title) {
-        super(editor, ProjectOpenCorePlugin.REPOSITORY_KIND);
-        this.editor = editor;
-        parts = new ArrayList<AbstractFormPagePart>();
+	public TicketEditorPage(TaskEditor editor, String title) {
+		super(editor, ProjectOpenCorePlugin.REPOSITORY_KIND, title);
+		this.editor = editor;
+		parts = new ArrayList<AbstractFormPagePart>();
 
-        ProjectOpenRepositoryConnector connector =
-            ProjectOpenCorePlugin.getDefault().getConnector();
-        client = connector.getClientManager().getClient(getTaskRepository());
-    }
+		ProjectOpenRepositoryConnector connector = ProjectOpenCorePlugin
+				.getDefault().getConnector();
+		client = connector.getClientManager().getClient(getTaskRepository());
+	}
 
     @Override
-    public void createFormContent(IManagedForm managedForm) {
-        super.createFormContent(managedForm);
-        form = managedForm.getForm();
-        toolkit = managedForm.getToolkit();
-        editorComposite = form.getBody();
-
-        // TODO consider using TableWrapLayout, it makes resizing much faster
-        GridLayout editorLayout = new GridLayout();
-        editorComposite.setLayout(editorLayout);
-        editorComposite.setLayoutData(new GridData(GridData.FILL_BOTH));
-
-        Display.getCurrent().asyncExec(new Runnable() {
-            public void run() {
-                downloadTicketAndRefresh();
-            }
-        });
+    public TaskEditor getEditor() {
+        return (TaskEditor) super.getEditor();
     }
 
-    private void createFormParts() {
-        TicketEditorHeaderPart headerPart = new TicketEditorHeaderPart(editor);
-        parts.add(headerPart);
-        if (ticket != null) {
-            parts.add(new TicketEditorAttributesPart(ticket, client, headerPart));
-        }
+    private ITask getTask() {
+        return getEditor().getTaskEditorInput().getTask();
     }
 
-    private void clearFormContent() {
-        for (AbstractFormPagePart part : parts) {
-            getManagedForm().removePart(part);
-        }
-        parts.clear();
-
-        // remove all of the old widgets so that we can redraw the editor
-        for (Control child : editorComposite.getChildren()) {
-            child.dispose();
-        }
+    private TaskRepository getTaskRepository() {
+        return getEditor().getTaskEditorInput().getTaskRepository();
     }
+	
+	@Override
+	public void createFormContent(IManagedForm managedForm) {
+		super.createFormContent(managedForm);
+		form = managedForm.getForm();
+		toolkit = managedForm.getToolkit();
+		editorComposite = form.getBody();
 
-    private void setBusy(boolean busy) {
-        getEditor().showBusy(busy);
-    }
+		// TODO consider using TableWrapLayout, it makes resizing much faster
+		GridLayout editorLayout = new GridLayout();
+		editorComposite.setLayout(editorLayout);
+		editorComposite.setLayoutData(new GridData(GridData.FILL_BOTH));
 
-    private void downloadTicketAndRefresh() {
-        Job job = new Job(NLS.bind("Retrieving ticket {0}...", getTask().getTaskId())) {
-            @Override
-            protected IStatus run(IProgressMonitor monitor) {
-                monitor.subTask("Retrieving ticket");
-                try {
-                    ticket = client.getTicket(Integer.valueOf(getTask().getTaskId()).intValue(), monitor);
-                } catch (Exception e) {
-                    return new Status(IStatus.ERROR, ProjectOpenUiPlugin.PLUGIN_ID, e.getMessage());
-                }
-                return new Status(IStatus.OK, ProjectOpenUiPlugin.PLUGIN_ID, null);
-            }
-        };
-        job.addJobChangeListener(new JobChangeAdapter() {
-            @Override
-            public void done(final IJobChangeEvent event) {
-                Display.getDefault().asyncExec(new Runnable() {
-                    public void run() {
-                        ticketUpdateCompleted();
-                    }
-                });
-            }
-        });
-        job.schedule();
-        setBusy(true);
-    }
+		Display.getCurrent().asyncExec(new Runnable() {
+			public void run() {
+				downloadTicketAndRefresh();
+			}
+		});
+	}
 
-    private void ticketUpdateCompleted() {
-        setBusy(false);
-        createInitialFormContent();
-    }
+	private void createFormParts() {
+		TicketEditorHeaderPart headerPart = new TicketEditorHeaderPart(editor);
+		parts.add(headerPart);
+		if (ticket != null) {
+			parts.add(new TicketEditorAttributesPart(ticket, client, headerPart));
+		}
+	}
 
-    private void createInitialFormContent() {
-        clearFormContent();
-        createFormParts();
+	private void clearFormContent() {
+		for (AbstractFormPagePart part : parts) {
+			getManagedForm().removePart(part);
+		}
+		parts.clear();
 
-        for (AbstractFormPagePart part : parts) {
-            getManagedForm().addPart(part);
-            part.initialize(getManagedForm());
-            part.createControl(editorComposite, toolkit);
-        }
+		// remove all of the old widgets so that we can redraw the editor
+		for (Control child : editorComposite.getChildren()) {
+			child.dispose();
+		}
+	}
 
-        form.layout(true, true);
-        form.reflow(true);
-        form.setRedraw(true);
-    }
+	private void setBusy(boolean busy) {
+		getEditor().showBusy(busy);
+	}
+
+	private void downloadTicketAndRefresh() {
+		Job job = new Job(NLS.bind("Retrieving ticket {0}...", getTask()
+				.getTaskId())) {
+			@Override
+			protected IStatus run(IProgressMonitor monitor) {
+				monitor.subTask("Retrieving ticket");
+				try {
+					ticket = client.getTicket(
+							Integer.valueOf(getTask().getTaskId()).intValue(),
+							monitor);
+				} catch (Exception e) {
+					return new Status(IStatus.ERROR,
+							ProjectOpenUiPlugin.PLUGIN_ID, e.getMessage());
+				}
+				return new Status(IStatus.OK, ProjectOpenUiPlugin.PLUGIN_ID,
+						null);
+			}
+		};
+		job.addJobChangeListener(new JobChangeAdapter() {
+			@Override
+			public void done(final IJobChangeEvent event) {
+				Display.getDefault().asyncExec(new Runnable() {
+					public void run() {
+						ticketUpdateCompleted();
+					}
+				});
+			}
+		});
+		job.schedule();
+		setBusy(true);
+	}
+
+	private void ticketUpdateCompleted() {
+		setBusy(false);
+		createInitialFormContent();
+	}
+
+	private void createInitialFormContent() {
+		clearFormContent();
+		createFormParts();
+
+		for (AbstractFormPagePart part : parts) {
+			getManagedForm().addPart(part);
+			part.initialize(getManagedForm());
+			part.createControl(editorComposite, toolkit);
+		}
+
+		form.layout(true, true);
+		form.reflow(true);
+		form.setRedraw(true);
+	}
 
 }
